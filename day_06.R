782da2deed9ef2feb8928d87fc2abaf9a49970d0
Cetin_G <ceting@gmx.at>
2021-12-07 07:18:44 +0100

    More eleganz solution for day06 based on transition matrix A

--- day_06.R
+++ day_06.R
@@ -1,10 +1,12 @@
 data06 <- read.table("Input/day06.txt", sep = ",")[1,]
 x <- as.numeric(sapply(0:8, \(i) sum(data06 == i)))
 
+A <- diag(9)[, c(2:9, 1)]
+A[1,7] <- 1
+
 #part 1---------
-for (i in seq_len(80)) x <- c(x[-1] + c(rep(0, 6), x[1], 0), x[1])
-sum(x)
+sum(x %*% Reduce(`%*%`, rep(list(A), 80)))
 
 #part 2--------
-for (i in seq_len(256 - 80)) x <- c(x[-1] + c(rep(0, 6), x[1], 0), x[1])
-print(sum(x), 12)
+print(sum(x %*% Reduce(`%*%`, rep(list(A), 256))), 12)
+#x %*% A is exactly the timings after one unit of time passes
