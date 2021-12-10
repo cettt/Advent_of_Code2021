0c2963215da5537f35a949919b8776905bca505c
Cetin_G <ceting@gmx.at>
2021-12-10 11:14:38 +0100

    Improvemnts and comments for Day10

--- day_10.R
+++ day_10.R
@@ -1,24 +1,19 @@
 data10 <- readLines("Input/day10.txt")
 
-points1 <- c(")" = 3L, "]" = 57L, "}" = 1197L, ">" = 25137L)
-points2 <- setNames(1:4, names(points1))
-completer <- c("(" = ")", "[" = "]", "{" = "}", "<" = ">")
+p1 <- c(")" = 3L, "]" = 57L, "}" = 1197L, ">" = 25137L)
+p2 <- c("(" = 1L, "[" = 2L,  "{" = 3L,    "<" = 4L)
 
-score_string <- function(x, part1 = TRUE) {
+pat <- "<>|\\{\\}|\\[\\]|\\(\\)"
+while (any(grepl(pat, data10))) data10 <- gsub(pat, "", data10)
+x <- strsplit(data10, "")
 
-  pat <- "<>|\\{\\}|\\[\\]|\\(\\)"
-  while (grepl(pat, x)) x <- gsub(pat, "", x)
-  y <- strsplit(x, "")[[1]]
-
-  if (part1) return(points1[y[y %in% c(")", "]", "}", ">")][1]])
-
-  tail(filter(points2[completer[rev(y)]], 5L, method = "rec"), 1)
-}
-
-#part1----
-sum(sapply(data10, score_string), na.rm = TRUE)
+#part1---------
+sum(sapply(x, \(y) p1[y[y %in% names(p1)][1]]), na.rm = TRUE)
 
 #part2------
-median(sapply(data10, score_string, part1 = FALSE), na.rm = TRUE)
+median(sapply(x, \(y) max(filter(p2[rev(y)], 5, method = "rec"))), na.rm = TRUE)
 
-
+# the idea is to remove matching pairs (), [], {}, <> until there are none left
+# for part1 we map the first closing parentheses to a score (p1) and take the sum
+# for part2 we map all opening brackets to a score (p2) in reverse order.
+#  the score is then computed using a linear filter
