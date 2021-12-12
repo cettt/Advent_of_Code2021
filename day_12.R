89f78265256f7ca7d3ecbbddf64082e9a9e870bf
Cetin_G <ceting@gmx.at>
2021-12-12 14:43:46 +0100

    Improved Day12

--- day_12.R
+++ day_12.R
@@ -1,39 +1,36 @@
 data12 <- read.table("Input/day12.txt", sep = "-")
 
-map <- cbind(c(data12[,1], data12[,2]), c(data12[,2], data12[,1]))
-map <- map[map[,2] != "start",]
-sc <- setdiff(map[grepl("[a-z]", map[,1]), 1], c("start", "end"))
+map <- setNames(c(data12[,1], data12[,2]), c(data12[,2], data12[,1]))
+map <- map[map != "start"]
 
-count_paths <- function(p, sc_vec, part1 = TRUE) {
+sc <- setdiff(map[grepl("[a-z]", map)], c("start", "end")) #small caves
+lookup <- integer()
 
-  if (p == "end") return(1)
-  if (p %in% sc) {
-    # print(p); print(sc_vec);
-    sc_vec[p] <- sc_vec[p] + 1L
-    }
+count_paths <- function(p, part1, sc_vec = setNames(integer(length(sc)), sc)) {
+
+  if (p == "end") return(1L)
+  if (p %in% sc) sc_vec[p] <- sc_vec[p] + 1L
+
   id <- paste(p, paste0(sc_vec, collapse = ""), sep = "_")
-  if (id %in% names(envir$res)) return(envir$res[id])
-  if (any(sc_vec > 1) & part1) {
-    res <- 0
-  } else if (!part1 & (any(sc_vec > 2) | sum(sc_vec > 1) > 1)) {
-    res <- 0
-  } else
-  {
-    ne <- setdiff(map[map[,1] == p, 2], names(sc_vec[sc_vec > 1]))
-    res <- if (length(ne) == 0) 0L else sum(sapply(ne,  count_paths, sc_vec = sc_vec, part1 = part1))
-  }
+  if (id %in% names(lookup)) return(lookup[id])
 
-  envir$res <- c(envir$res, setNames(res, id))
+  invalid <- any(sc_vec > 2) | sum(sc_vec > 1) > 1 | (part1 & any(sc_vec > 1))
+  res <- if (invalid) 0L else sum(sapply(map[names(map) == p], count_paths, part1, sc_vec))
+
+  lookup <<- c(lookup, setNames(res, id))
   return(res)
-
 }
 
-envir <- environment()
-envir$res <- NULL
-sc_vec <- setNames(rep(0, length(sc)), sc)
-count_paths("start", sc_vec, TRUE)
-
+#part1-----
+count_paths("start", part1 = TRUE)
 
 #part2-----------
-envir$res <- NULL
-count_paths("start", sc_vec, FALSE)
+lookup <- integer()
+count_paths("start", part1 = FALSE)
+
+#first we create a vector which is our map: we use the names attribute
+#   we remove start from the map such that there cannot be any paths which go back to start
+#   we make a list of the small caves
+#   the main function is recursive: in each step we update the list of small caves visited (sv_vec)
+#    we use sv_vec to determine if a path is valid or not.
+#    we create a global lookup which saves all subresults.
