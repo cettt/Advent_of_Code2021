4966bb0e4deff1d8fc52a56fcbb7d9fa21b2cbe8
Cetin_G <ceting@gmx.at>
2021-12-17 10:54:20 +0100

    Small improvement in Day 16

--- day_16.R
+++ day_16.R
@@ -6,35 +6,33 @@ map <- sort(apply(expand.grid(0:1, 0:1, 0:1, 0:1), 1,  paste, collapse = ""))
 map <- setNames(map, c(0:9, LETTERS[1:6]))
 
 bits <- as.integer(unlist(strsplit(map[strsplit(data16, "")[[1]]], "")))
-idx <- 1L
 sv <- 0L #sum of versions
 
 find_value <- function() {
 
-  sv <<- sv + bin2int(bits[idx + 0:2])# update sum of versions
-  id <- bin2int(bits[idx + 3:5]) # ID
+  sv <<- sv + bin2int(bits[1:3])# update sum of versions
+  id <- bin2int(bits[4:6]) # ID
 
   if (id == 4L) { #literal value
-    z <- bits[(idx + 6):length(bits)]
-    nval <- which(z[seq_along(z) %% 5L == 1L] == 0)[1]
-    idx <<- idx + 6L + nval*5L
-    return(bin2int(z[seq_along(z) %% 5L != 1L & seq_along(z) <= nval*5]))
-
-  } else { #operation
-    out_fun <- list(sum, prod, min, max, `>`, `<`, `==`)[[id + (id < 4)]]
-
-    if (bin2int(bits[idx + 6L]) == 0) {#length
-      idx_tar <- idx + 22L + bin2int(bits[idx + 6L + 1:15])  #target index
-      idx <<-  idx + 22L
-      val <- integer()
-      while (idx < idx_tar) val <- c(val, find_value()) #stop when target is reached
-    } else {#number of packages
-      n_pack <- bin2int(bits[idx + 6L + 1:11])
-      idx <<- idx + 18L
-      val <- sapply(seq_len(n_pack), \(k) find_value())
-    }
-    return(if (id < 4L) out_fun(val) else out_fun(val[1], val[2]))
+    z <- bits[7:length(bits)]
+    nval <- which(z[seq_along(z) %% 5L == 1L] == 0L)[1L]
+    bits <<- bits[-seq_len(6L + nval*5L)]
+    return(bin2int(z[seq_along(z) %% 5L != 1L & seq_along(z) <= nval*5L]))
   }
+  else if (bits[7L] == 0L) { #operation__length
+    tar_len <- length(bits) - 22L - bin2int(bits[7L + seq_len(15L)])  #target length
+    bits <<- bits[-seq_len(22L)]
+    val <- integer()
+    while (length(bits) > tar_len) val <- c(val, find_value()) #stop when target is reached
+  }
+  else {#number of packages
+    n_pack <- bin2int(bits[7L + seq_len(11L)])
+    bits <<- bits[-seq_len(18L)]
+    val <- sapply(seq_len(n_pack), \(k) find_value())
+  }
+
+  out_fun <- list(sum, prod, min, max, `>`, `<`, `==`)[[id + (id < 4)]]
+  return(if (id < 4L) out_fun(val) else out_fun(val[1], val[2]))
 }
 
 #part2 -------
