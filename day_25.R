0dddde1fb643ed4efe0534556a86ae68ca95a716
Cetin_G <ceting@gmx.at>
2021-12-28 16:44:51 +0100

    Optimized Day 25

--- day_25.R
+++ day_25.R
@@ -1,20 +1,25 @@
-day25 <- as.character(do.call(rbind, strsplit(readLines("Input/day25.txt"), "")))
+data25 <- as.character(do.call(rbind, strsplit(readLines("Input/day25.txt"), "")))
 
-z <- complex(real = rep(0:138, each = 137), imaginary = rep(0:136, 139))
-east <- z[day25 == ">"]
-sout <- z[day25 == "v"]
-
-for (k in seq_len(1e3)) {
-
-  east2 <- (Re(east + 1) %% 139 + Im(east) * 1i)
-  idx <- east2 %in% c(east, sout)
-  east[!idx] <- east2[!idx]
-
-  sout2 <- Re(sout) + ((Im(sout) + 1) %% (137)) * 1i
-  idx2 <- sout2 %in% c(east, sout)
-  sout[!idx2] <- sout2[!idx2]
-
-  if (all(c(idx, idx2))) break
+neighbor_k <- function(k) {
+  m <- k %% 137L
+  res <- k + c( -1L + if (m == 1L) 137L else 0L, 1L - if (m == 0) 137 else 0L)
+  c(res, (k - 138L) %% (137L * 139L) + 1L, (k + 136L) %% (137L * 139L) + 1L)
 }
 
-k
+lu <- sapply(seq_along(data25), neighbor_k)
+map0 <- unname(c(">" = 1L, "v" = 2L, "." = 0L)[data25])
+
+update_map <- function(x) {
+  x1 <- ifelse(x == 0L & x[lu[3, ]] == 1L, 1L,
+               ifelse(x == 0L, 0L, ifelse(x == 1L & x[lu[4, ]] == 0L, 0L, x)))
+
+  ifelse(x1 == 0L & x[lu[1,]] == 2L, 2L, ifelse(x1 == 2L & x1[lu[2, ]] == 0L, 0L, x1))
+}
+
+for (steps in 1:1e3) {
+  map1 <- update_map(map0)
+  if (all(map1 == map0)) break
+  map0 <- map1
+}
+steps
+
